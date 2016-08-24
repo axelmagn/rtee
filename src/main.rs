59d5a805b6705d7e93a59c4428a63e6a35754988
Axel Magnuson <axelmagn@gmail.com>
2016-08-24 15:49:19 -0600

    Write a basic, functional tee program
        This does not implement error handling yet.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ extern crate rustc_serialize;
 extern crate docopt;
 
 use docopt::Docopt;
+use std::io::prelude::*;
+use std::io;
+use std::fs::{OpenOptions, File};
+
+
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
@@ -17,12 +22,11 @@ Options:
   -h --help                 display this help and exit.
 ";
 
+
 #[derive(Debug, RustcDecodable)]
 struct Args {
     arg_file: Vec<String>,
     flag_append: bool,
-    flag_version: bool,
-    flag_help: bool,
 }
 
 
@@ -38,5 +42,45 @@ fn main() {
                             // -> Result<Args, Error>
                              .unwrap_or_else(|e| e.exit());
                             // -> Args
-    println!("{:?}", args);
+                            
+
+
+    // set up open opts
+    let append = args.flag_append;
+    let open_opts = { 
+        let mut o = OpenOptions::new();
+        o.read(false)
+         .write(true)
+         .create(true)
+         .append(append)
+         .truncate(!append);
+        o
+    };
+
+    // Open files for writing
+    let mut files: Vec<File> = args.arg_file.iter()
+                                     .map(|p| open_opts.open(p).unwrap())
+                                     .collect();
+
+    // open stderr and stdout for writing
+    // let stderr = io::stderr();
+    let mut stdout = io::stdout();
+
+    // consume stdin
+    io::stdin().bytes()
+        // handle errors by printing them to stderr
+        .map(|b| b.unwrap())
+        // write byte to each file
+        // (ownership fail)
+        .map(|b| {
+            // write to each file
+            // fail loudly for now
+            files.iter_mut()
+                .map(|f: &mut File| f.write(&[b]).unwrap())
+                .last();
+            b
+        })
+        // write to stdout
+        .map(|b| stdout.write(&[b]).unwrap())
+        .last();
 }
